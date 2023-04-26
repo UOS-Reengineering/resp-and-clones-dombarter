193bca5b575492b4e2a46e0095f5f4290a2d9657
Dom Barter <dominicbarter@gmail.com>
2023-04-26 12:03:08 +0100

    Jaccard similarity

--- src/main/java/example/project/CodeClones/JaccardSimilarity.java
+++ src/main/java/example/project/CodeClones/JaccardSimilarity.java
@@ -6,8 +6,15 @@ import java.util.Set;
 public class JaccardSimilarity {
 
     public static double computeJaccardSimilarity(Set<String> set1, Set<String> set2) {
-        // TODO: implement this function to compute the Jaccard similarity between two string arrays
-        return -1.0;
+
+        Set intersection = new HashSet(set1);
+        intersection.retainAll(set2);
+
+        Set union = new HashSet(set1);
+        union.addAll(set2);
+
+        return (double)intersection.size() / (double)union.size();
+
     }
 
 }
